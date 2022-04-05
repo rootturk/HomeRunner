fa223c892035c669f4e92d01f9dff0ccb8ce8719
“rootturk” <akin@akinabdullahoglu.com>
2022-04-05 11:45:09 +0300

    changed project folder structure.

--- model/blockedUser.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const blockedUserSchema = new mongoose.Schema({
-    username: {type:String, default:null},
-    blocked_username: {type:String, default:null},
-    created: { type: Date, default: Date.now() }
-})
-
-module.exports = mongoose.model('blockedUser', blockedUserSchema)
